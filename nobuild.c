22554b03535d4e8d45dc270229196a8471c10990
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-04-10 18:22:10 +0530

    nobuild can now build and run tests

--- nobuild.c
+++ nobuild.c
@@ -1,13 +1,17 @@
+#include <string.h>
+
 #define NOBUILD_IMPLEMENTATION
 #include "nobuild.h"
 
 #define CC "gcc"
-#define CFLAGS "-Wall", "-Wextra", "-g", "-O3", "-std=c11", "-I./src"
+#define CFLAGS "-Wall", "-Wextra", "-g", "-O3", "-std=c11", "-I./src", "-lcrypto"
 
 #define SRC_DIR "./src"
+#define TEST_DIR "./test"
 #define BUILD_DIR "./build"
 #define OUT_DIR BUILD_DIR "/out"
 #define BIN_DIR BUILD_DIR "/bin"
+#define TEST_BIN_DIR BUILD_DIR "/test"
 
 #define SERVER_BIN "yard-server"
 #define CLIENT_BIN "yard-client"
@@ -86,6 +90,29 @@ void make_dirs() {
     MKDIRS(BUILD_DIR);
     MKDIRS(OUT_DIR);
     MKDIRS(BIN_DIR);
+    MKDIRS(TEST_BIN_DIR);
+}
+
+void build_test() {
+    Cstr_Array cmd_line = cstr_array_make(CC, CFLAGS, "-I./test", "-o", PATH(TEST_BIN_DIR, "yard_test"),
+                                          PATH(TEST_DIR, "main_test.c"), NULL);
+
+    FOREACH_FILE_IN_DIR(f, OUT_DIR, {
+        if (!(ENDS_WITH(f, "client.o") || ENDS_WITH(f, "server.o") || ENDS_WITH(f, ".") || ENDS_WITH(f, ".."))) {
+            cmd_line = cstr_array_append(cmd_line, PATH(OUT_DIR, f));
+        }
+    });
+
+    Cmd cmd = {
+        .line = cmd_line,
+    };
+
+    INFO("CMD: %s", cmd_show(cmd));
+    cmd_run_sync(cmd);
+}
+
+void run_test() {
+    CMD(TEST_BIN_DIR "/yard_test");
 }
 
 int main(int argc, char **argv) {
@@ -96,4 +123,9 @@ int main(int argc, char **argv) {
     compile_ds();
     build_server();
     build_client();
+
+    if (argc == 2 && strncmp(argv[1], "test", 4) == 0) {
+        build_test();
+        run_test();
+    }
 }

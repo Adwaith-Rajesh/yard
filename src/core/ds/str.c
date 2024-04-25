6fe530f485b4a7474043571b916d5d32e17d4d00
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-04-25 18:11:56 +0530

    str: added function to create, append, print and free for strings

---
+++ src/core/ds/str.c
@@ -0,0 +1,48 @@
+#include "core/ds/str.h"
+
+#include <stdio.h>
+#include <strings.h>
+
+Str *str_create(void *(*allocator)(size_t),
+                void (*deallocator)(void *),
+                void *(*reallocator)(void *, size_t)) {
+    Str *new_str = allocator(sizeof(Str));
+    new_str->str = allocator(sizeof(char) * 2);
+    new_str->_size = 2;
+    new_str->len = 0;
+
+    // init the string
+    bzero(new_str->str, new_str->_size);
+
+    new_str->allocator = allocator;
+    new_str->deallocator = deallocator;
+    new_str->reallocator = reallocator;
+
+    return new_str;
+}
+
+void str_free(Str *str) {
+    str->deallocator(str->str);
+    str->deallocator(str);
+}
+
+void str_print(Str *str) {
+    printf("%s", str->str);
+}
+
+Str *str_append_charp(Str *str, const char c) {
+    // printf("str_len %zu\n", str->len);
+    if (str->len == (str->_size - 1)) {
+        // printf("in if\n");
+        str->str = str->reallocator(str->str, str->_size * 2);
+
+        size_t size_prev = str->_size;
+
+        str->_size *= 2;
+
+        // init the new allocated chunk
+        bzero(&(str->str[size_prev]), str->_size - size_prev);
+    };
+
+    str->str[str->len++] = c;
+}
---
+++ src/core/ds/str.h
@@ -0,0 +1,32 @@
+#ifndef YARD_DS_STR_H
+#define YARD_DS_STR_H
+
+#include <stddef.h>
+
+typedef struct {
+    char *str;
+    size_t len;
+    size_t _size;
+
+    void *(*allocator)(size_t);
+    void (*deallocator)(void *);
+    void *(*reallocator)(void *, size_t);
+} Str;
+
+// create an empty Str
+// by default allocated size with 16
+Str *str_create(void *(*allocator)(size_t), void (*deallocator)(void *), void *(*reallocator)(void *, size_t));
+
+// create a string from char *
+Str *str_conv_charp(Str *str, const char *p);
+
+// add a character to the given string
+Str *str_append_charp(Str *str, const char c);
+
+// print a given Str
+void str_print(Str *str);
+
+// free the string
+void str_free(Str *str);
+
+#endif

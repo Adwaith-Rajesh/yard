adae51eab5593ee47d32a883ce6aabeafcdcc4b6
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-04-28 01:45:04 +0530

    added a basic structure for defining commands

--- src/core/cmd/cmds.c
+++ src/core/cmd/cmds.c
@@ -3,12 +3,40 @@
 #include <stdio.h>
 
 #include "core/cmd/parser.h"
-#include "yard.h"
+#include "mctx.h"
 
-CMD_SIG(get) {
-    printf("this is the get command\n");
+// satatic int CMD(YardMasterCtx *mctx, ParserCtx *pctx, )
+
+DEFINE_CMD(get) {
+    res->ok = 1;
+    res->result_type = R_INT;
+    res->_int = 2;
 }
 
-CMD_SIG(set) {
+DEFINE_CMD(set) {
+    (void)mctx;
+    (void)pctx;
     printf("this is the set command\n");
+}
+
+DEFINE_CMD(del) {
+    (void)mctx;
+    (void)pctx;
+    printf("this is the del commands\n");
+}
+
+// wrappers the contains extra info about the commands
+
+CMD_WRAP(get) {
+    CHECK_HELP("Usage: get keyname\n");
+    ENFORCE_ARG_COUNT(1, {
+        SET_ERROR("Please provide key name\n");
+        return;
+    });
+
+    EXE_CMD(get);
+}
+
+CMD_WRAP(set) {
+    CHECK_HELP("Usage: set keyname value\n");
 }
--- src/core/cmd/cmds.h
+++ src/core/cmd/cmds.h
@@ -2,12 +2,49 @@
 #define YARD_CORE_CMD_CMDS_H
 
 #include "core/cmd/parser.h"
-#include "yard.h"
+#include "mctx.h"
 
 // creates the function signature for the command
-#define CMD_SIG(CMD) int CMD(YardMasterCtx *mctx, ParserCtx *pctx)
+#define DEFINE_CMD(CMD) static void CMD(YardMasterCtx *mctx, ParserCtx *pctx, CmdResult *res)
 
-CMD_SIG(get);
-CMD_SIG(set);
+#define CMD_WRAP(CMD) void CMD##_wrap(YardMasterCtx *mctx, ParserCtx *pctx, CmdResult *res)
+
+#define CHECK_HELP(H_MSG)                       \
+    do {                                        \
+        if (mctx == NULL && pctx == NULL) {     \
+            res->ok = 1;                        \
+            res->result_type = R_HELP;          \
+            str_clear(res->emsg);               \
+            str_append_charp(res->emsg, H_MSG); \
+        }                                       \
+    } while (0)
+
+#define SET_ERROR(ERR_MSG)                    \
+    do {                                      \
+        res->ok = 0;                          \
+        res->result_type = R_ERROR;           \
+        str_clear(res->emsg);                 \
+        str_append_charp(res->emsg, ERR_MSG); \
+    } while (0)
+
+#define ENFORCE_ARG_COUNT(COUNT, B)           \
+    do {                                      \
+        if (pctx->cmd->size != (COUNT + 1)) { \
+            B;                                \
+        }                                     \
+    } while (0)
+
+#define EXE_CMD(CMD) CMD(mctx, pctx, res)
+
+// get key
+CMD_WRAP(get);
+
+// set key val
+CMD_WRAP(set);
+
+// del key
+CMD_WRAP(del);
 
 #endif
+
+// context can store the help message
